92d599181e251b13107d0549145ff3d5e0528d5b
LM-01 <mollinedo.lester@gmail.com>
2021-06-05 20:03:25 -0400

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React from "react";
+import './css/dark-theme.css'
 
 function App() {
   return (
-    <div>
+    <>
+    <div className='background-header'/>
+    <div className='grid-container'>
+      <div className='header'>
+        <div className='grid-row book-ends'><h1>TODO</h1> <span>ICON</span></div>
+        <div className='grid-row task_input mt-md'>
+          <input className='task-input'></input>
+        </div>
+      </div>
+      <div className='task-list mt-md'>
+        <div className="task-container">
+          <div className="">X</div>
+          <div className="">Actual Task</div>
+          <div className="">X</div>
+        </div>
+        <div className="task-container">New Task</div>
+        <div className="task-container">New Task</div>
+      </div>
     </div>
+    </>
   );
 }
 
